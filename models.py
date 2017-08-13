035baad767152a11cb2f979c9c12f7a9882c5ca3
Mohammed Salman <m.salman@zinkki.com>
2017-08-13 10:21:32 +0300

    Add the functionality to save and load from databade

--- models.py
+++ models.py
@@ -2,30 +2,37 @@ from scipy.interpolate import interp1d
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 import tinydb
+db = tinydb.TinyDB('airfoils.json')
+objects = tinydb.Query()
+
 
 class AirFoil():
 
-    def __init__(self, description, x_points, y_positive, y_negative):
+    def __init__(self, description,
+                 x_points, y_positive, y_negative, name='default'):
         self.coordinates = OrderedDict(
             zip(x_points, list(zip(y_positive, y_negative)))
         )
         self.description = description
+        self.name = name
 
     def __len__(self):
         return len(self.coordinates)
 
     def interpolate(self, x_points):
-        "Take a list of points in the x axis and interpolate y values for it."
+        """
+        Take a list of points in the x axis and interpolate y values for it.
+        """
         needed_points = [
             point for point in x_points if point not in self.x_coordinates
         ]
         if not needed_points:
             return
         f_positive = interp1d(
-            self.x_coordinates, self.y_coordinates_positive()
+            self.x_coordinates, self.y_coordinates_positive
         )
         f_negative = interp1d(
-            self.x_coordinates, self.y_coordinates_negative()
+            self.x_coordinates, self.y_coordinates_negative
         )
         for point in needed_points:
             self.coordinates[point] = [
@@ -57,9 +64,11 @@ class AirFoil():
                 y_list.append(list(y))
         return y_list
 
+    @property
     def y_coordinates_positive(self):
         return self.y_coordinates('positive')
 
+    @property
     def y_coordinates_negative(self):
         return self.y_coordinates('negative')
 
@@ -67,5 +76,115 @@ class AirFoil():
         plt.plot(self.x_coordinates, self.y_coordinates())
         plt.show()
 
-    def save(self):
+    def save(self, db=tinydb.TinyDB('airfoils.json')):
+        """
+        Save the airfoil object to the database.
+
+        It will update the airfoil if the name is already available
+        in the database. If the name is new, it will create it.
+        """
+        previous_object = db.contains(objects.name == self.name)
+        if previous_object:
+            db.update({
+                'x_coordinates': self.x_coordinates,
+                'y_coordinates_pos': self.y_coordinates_positive,
+                'y_coordinates_neg': self.y_coordinates_negative,
+                'description': self.description
+            },
+                objects.name == self.name
+            )
+        # The object can not be found
+        db.insert(
+            {
+                'name': self.name,
+                'x_coordinates': self.x_coordinates,
+                'y_positive': self.y_coordinates_positive,
+                'y_negative': self.y_coordinates_negative,
+                'description': self.description
+            }
+        )
         pass
+
+
+def airfoil_from_lednicer(data, name):
+    middle_point = int((len(data) - 1) / 2) + 1
+    # End of file.
+    eof = len(data) - 1
+    x = []
+    y_positive = []
+    y_negative = []
+    # Get the first half of the file,
+    # starting from 3 row until the middle point
+    for row in data[3:middle_point]:
+        try:
+            # First item of the row is the x point,
+            # the second is the positive y point.
+            x.append(float(row.split()[0]))
+            y_positive.append(float(row.split()[1]))
+        except ValueError:
+            pass
+    # Get the second half of the file to get the negative y points.
+    for row in data[(middle_point + 1):eof]:
+        try:
+            y_negative.append(float(row.split()[1]))
+        except ValueError:
+            pass
+    return AirFoil(
+        description=data[0], x_points=x,
+        y_positive=y_positive, y_negative=y_negative,
+        name=name
+    )
+
+
+def airfoil_from_selig(data, name):
+    middle_point = int((len(data) - 1) / 2)
+    # End of file
+    eof = len(data) - 1
+    x = []
+    y_positive = []
+    y_negative = []
+    # Flipping coordinations, selig coordinates descends.
+    flipped_first_half = data[middle_point::-1]
+    for row in flipped_first_half[:-1]:
+        try:
+            x.append(float(row.split()[0]))
+            y_positive.append(float(row.split()[1]))
+        except ValueError:
+            pass
+    for row in data[middle_point:eof]:
+        try:
+            y_negative.append(float(row.split()[1]))
+        except ValueError:
+            pass
+    return AirFoil(
+        description=data[0], x_points=x,
+        y_positive=y_positive, y_negative=y_negative,
+        name=name
+    )
+
+
+def airfoil_from_data(input_data, name):
+    # Variable is not empty
+    if input_data:
+        # determine file format: selig or Lednicer
+        third_line = input_data[2]
+        first_word_fourth_line = input_data[3].split()[0]
+        if not(third_line):
+            # The dat format is Lednicer.
+            return airfoil_from_lednicer(input_data, name)
+        else:
+            # The dat format is selig.
+            return airfoil_from_selig(input_data, name)
+
+
+def airfoil_from_database(name, db=tinydb.TinyDB('airfoils.json')):
+    """Return an airfoil object from database."""
+    airfoil_attributes = db.get(objects.name == name)
+    if airfoil_attributes:
+        return AirFoil(**airfoil_attributes)
+    else:
+        raise LookupError(
+            'The AirFoil %s can not be found in the database %s' %
+            name,
+            db
+        )
--- tests.py
+++ tests.py
@@ -1,15 +1,41 @@
 """Tests for an_airfoil main functionalities."""
 import unittest
-from converters import airfoil_from_data as afd
+from models import airfoil_from_data as afd
+from models import AirFoil, airfoil_from_database
 from equity import equate
+from tinydb import TinyDB, Query
+objects = Query()
 
 
 class TestAirFoil(unittest.TestCase):
     """Tests for AirFoil class and methods."""
+    def setUp(self):
+        """Construct test needed objects."""
+        from tinydb.storages import MemoryStorage
+        self.test_db = TinyDB(storage=MemoryStorage)
 
-    def test_wings_load(self):
-        """Test if wings loading works as expected."""
-        pass
+    def test_airfoil_from_db(self):
+        """Test if wings loading from db works as expected."""
+        air_foil_attributes = {
+            'name': 'test',
+            'x_points': [1, 2, 3, 4],
+            'y_positive': [0.1, 0.2, 0.3, 0.4],
+            'y_negative': [10, 20, 30, 40],
+            'description': 'description_test'
+        }
+        self.test_db.insert(air_foil_attributes)
+        airfoil_object = airfoil_from_database(name='test', db=self.test_db)
+        self.assertEqual(
+            airfoil_object.x_coordinates, air_foil_attributes['x_points']
+        )
+        self.assertEqual(
+            airfoil_object.y_coordinates_positive,
+            air_foil_attributes['y_positive']
+        )
+        self.assertEqual(
+            airfoil_object.y_coordinates_negative,
+            air_foil_attributes['y_negative']
+        )
 
     def test_wing_from_selig(self):
         """Test that selig format can be parsed to functional objects."""
@@ -23,14 +49,14 @@ class TestAirFoil(unittest.TestCase):
             0.56526, 0.62941, 0.69134, 0.75, 0.80438, 0.85355, 0.89668,
             0.93301, 0.96194, 0.98296, 0.99572, 1.0
         ]
-        air_foil = afd(selig_file)
+        air_foil = afd(selig_file, 'Test')
         self.assertEqual(air_foil.x_coordinates, fixture_x_coordinates)
 
     def test_wing_from_lednicer(self):
         """Test that lednicer format can be parsed to functional objects."""
         with open('tests_fixtures/lednicer.txt') as f:
             lednicer_file = f.read().split('\n')
-        air_foil = afd(lednicer_file)
+        air_foil = afd(lednicer_file, name='Tesst')
         fixture_x_coordinates = [0.0, 0.00107, 0.00428, 0.00961, 0.01704, 1.0]
         self.assertEqual(air_foil.x_coordinates, fixture_x_coordinates)
 
@@ -46,24 +72,69 @@ class TestAirFoil(unittest.TestCase):
         for air_foil_name, file_name in files.items():
             with open('tests_fixtures/%s' % file_name) as f:
                 airfoil_file = f.read().split('\n')
-                airfoils[air_foil_name] = afd(airfoil_file)
+                airfoils[air_foil_name] = afd(airfoil_file, name=file_name)
         equate(airfoils['first'], airfoils['second'])
         self.assertEqual(
             airfoils['first'].x_coordinates,
             airfoils['second'].x_coordinates
         )
+        airfoils['first'].save()
+        airfoils['second'].save()
+
+    def test_save_new_object(self):
+        """When the object name is new, new object in db should be created."""
+        test_airfoil = AirFoil(
+            name='test',
+            x_points=[1, 2, 3, 4],
+            y_positive=[0.1, 0.2, 0.3, 0.4],
+            y_negative=[10, 20, 30, 40],
+            description='description_test'
+        )
+        test_airfoil.save(db=self.test_db)
+        airfoil_attribs_from_db = self.test_db.get(objects.name == 'test')
+        self.assertEqual(
+            test_airfoil.x_coordinates,
+            airfoil_attribs_from_db['x_coordinates']
+        )
+        self.assertEqual(
+            test_airfoil.y_coordinates_positive,
+            airfoil_attribs_from_db['y_positive']
+        )
+        self.assertEqual(
+            test_airfoil.y_coordinates_negative,
+            airfoil_attribs_from_db['y_negative']
+        )
+
+    def test_save_for_old_object(self):
+        """When the object name is old, old object should be updated."""
+        # Create an intry in the database with the name test.
+        self.test_db.insert({'name': 'test', 'x_points': [0, 0, 0]})
+        test_airfoil = AirFoil(
+            name='test',
+            x_points=[1, 2, 3, 4],
+            y_positive=[0.1, 0.2, 0.3, 0.4],
+            y_negative=[10, 20, 30, 40],
+            description='description_test'
+        )
+        test_airfoil.save(db=self.test_db)
+        airfoil_attribs_from_db = self.test_db.get(objects.name == 'test')
+        self.assertEqual(
+            test_airfoil.x_coordinates,
+            airfoil_attribs_from_db['x_coordinates']
+        )
+        self.assertEqual(
+            test_airfoil.y_coordinates_positive,
+            airfoil_attribs_from_db['y_coordinates_pos']
+        )
+        self.assertEqual(
+            test_airfoil.y_coordinates_negative,
+            airfoil_attribs_from_db['y_coordinates_neg']
+        )
+
+    def tearDown(self):
+        """Delete test objects from test database."""
+        self.test_db.purge()
+
 
 if __name__ == '__main__':
     unittest.main()
-
-def test():
-    input_file = input('Please input file name: ')
-    f = open(input_file, 'r').read().split('\n')
-    return afd(f)
-
-
-
-def test4():
-    wing1 = test2()
-    wing2 = test3()
-    return equate(wing1, wing2)
